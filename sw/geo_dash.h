ce418915a9a5850f544615c7998784a2aa576d64
Sasha Isler <sashaisler@gmail.com>
2025-05-01 12:32:23 -0400

    added missing defines to header

--- sw/geo_dash.h
+++ sw/geo_dash.h
@@ -22,9 +22,15 @@ typedef struct {
 
 #define GEO_DASH_MAGIC 'q'
 
-#define WRITE_X_SHIFT _IOW(GEO_DASH_MAGIC, 0, geo_dash_arg_t *)
-
-#define WRITE_AUDIO_FIFO _IOW(GEO_DASH_MAGIC, 9, geo_dash_arg_t *)
+#define WRITE_X_SHIFT 		 _IOW(GEO_DASH_MAGIC, 0, geo_dash_arg_t *)
+#define WRITE_PLAYER_Y_POS   _IOW(GEO_DASH_MAGIC, 1, geo_dash_arg_t *)
+#define WRITE_BACKGROUND_R   _IOW(GEO_DASH_MAGIC, 2, geo_dash_arg_t *)
+#define WRITE_BACKGROUND_G   _IOW(GEO_DASH_MAGIC, 3, geo_dash_arg_t *)
+#define WRITE_BACKGROUND_B   _IOW(GEO_DASH_MAGIC, 4, geo_dash_arg_t *)
+#define WRITE_MAP_BLOCK      _IOW(GEO_DASH_MAGIC, 5, geo_dash_arg_t *)
+#define WRITE_FLAGS          _IOW(GEO_DASH_MAGIC, 6, geo_dash_arg_t *)
+#define WRITE_OUTPUT_FLAGS   _IOW(GEO_DASH_MAGIC, 7, geo_dash_arg_t *)
+#define WRITE_AUDIO_FIFO 	 _IOW(GEO_DASH_MAGIC, 9, geo_dash_arg_t *)
 
 
 #endif
