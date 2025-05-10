0107264b46fbedd481303b4b6b18c167b5fdbb7d
UjirYed <rd3054@columbia.edu>
2025-05-10 16:00:05 -0400

    fix to hex2bin to ignore comments

--- hw/gd-tiles/hex2bin.py
+++ hw/gd-tiles/hex2bin.py
@@ -9,6 +9,9 @@ print(sys.argv[0])
 print(file_name)
 with open(f"{sys.argv[1]}") as infile, open(f"{file_name}.bin", "wb") as outfile:
     for line in infile:
+        # Remove comments
+        if '//' in line:
+            line = line.split('//')[0].strip()
         # Remove any extra spaces or newline characters
         clean_line = line.strip().replace("  ", " ").replace("\t", " ")
         if not clean_line:
