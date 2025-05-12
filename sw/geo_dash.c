6574621f884c2a0ce5ab4b8a9e528109be48603a
Sasha Isler <sashaisler@gmail.com>
2025-05-12 15:33:34 -0400

    added y pos to geo_dash

--- sw/geo_dash.c
+++ sw/geo_dash.c
@@ -41,7 +41,6 @@
 
 #define DRIVER_NAME "geo_dash"
 // Assuming that we have 16-bit registers.
-#define PLAYER_Y_POS(base)   ((base) + 0x00)  // 16-bit
 #define X_SHIFT(base)        ((base) + 0x02)  // 16-bit
 
 #define TILEMAP(base)   ((base))
@@ -51,6 +50,8 @@
 #define FLAGS(base)          ((base) + 0x0C)  // lower 8 bits used
 #define OUTPUT_FLAGS(base)   ((base) + 0x0E)  // lower 8 bits used
 #define SCROLL_OFFSET(base)  ((base) + 0x10)  // 16-bit  - added for tile scroll
+#define PLAYER_Y_POS(base) ((base) + 0x3002)  // 16-bit register
+
 
 /*
 Information about our geometry_dash device. Acts as a mirror of hardware state.
@@ -112,7 +113,6 @@ static uint8_t read_tileset(int tile_no, int pixel_no)
 }
 
 
-
 static void write_player_y_position(unsigned short *value) {
     iowrite16(*value, PLAYER_Y_POS(geo_dash_dev.virtbase));
 }
@@ -157,6 +157,7 @@ static long geo_dash_ioctl(struct file *f, unsigned int cmd, unsigned long arg)
             if (copy_to_user((geo_dash_arg_t *) arg, &vla, sizeof(vla)))
                   return -EFAULT;
         }
+		break;
         case WRITE_PALETTE:
             pr_info("writing to palette\n");            
             write_palette(&vla.rgb, vla.color_index);
@@ -169,6 +170,7 @@ static long geo_dash_ioctl(struct file *f, unsigned int cmd, unsigned long arg)
             if (copy_to_user((geo_dash_arg_t *) arg, &vla, sizeof(vla)))
                 return -EFAULT;
         }
+		break;
         case WRITE_TILESET: ;
 //            pr_info("writing to tile set\n");
             /* this should write 32x32 bytes to the location requested*/
